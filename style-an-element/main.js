dd3f6bac16f331fe20b017333bc58292e9be2b83
cagataysen <orhancagataysen@outlook.com>
2024-01-06 17:18:59 +0300

    style-an-element added

---
+++ style-an-element/main.js
@@ -0,0 +1,13 @@
+let bodyElement = document.querySelector("body");
+bodyElement.style.backgroundColor = "#201F2E";
+
+document.querySelector(".heading").style.fontFamily = "Roboto";
+
+/*
+
+querySelector is choosing the first or related element tag, other one is choosing all
+we start with .heading cuz its a className
+document.querySelector
+document.getElementsByTagName 
+
+*/
