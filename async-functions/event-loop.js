5859f6693ddbf234048a0fe691f2b803cec29a95
cagataysen <orhancagataysen@outlook.com>
2024-01-10 20:30:39 +0300

    event loop added

---
+++ async-functions/event-loop.js
@@ -0,0 +1,26 @@
+console.log("I'm learning about");
+
+for (let idx = 0; idx < 999999999; idx++) {}
+
+// The second console.log() statement is
+// delayed by the for loop's execution
+console.log("the Event Loop");
+
+/* The code above is synchronise one */
+
+console.log("I’m learning about");
+setTimeout(() => {
+  console.log("Event Loop");
+}, 2000);
+console.log("the");
+/*######################################################################################################*/
+
+/*Event Loop*/
+console.log("This is the first line of code in app.js.");
+
+function usingsetTimeout() {
+  console.log("I'm going to be queued in the Event Loop.");
+}
+setTimeout(usingsetTimeout, 3000);
+
+console.log("This is the last line of code in app.js.");
